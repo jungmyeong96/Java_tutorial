1aa8ad87f71aac0d8b6a7c9cc56d7fbf0b45327e
한중명 <gkswndaud@gmail.com>
2023-02-01 15:04:06 +0900

    study garbage collection

--- src/classObjects/Student.java
+++ src/classObjects/Student.java
@@ -13,6 +13,14 @@ public class Student {
 		System.out.println(studentName + "," + address);
 	}
 	
+	public String getStudentName() {
+		return studentName;
+	}
+	
+	public void setStudentName(String name) {
+		studentName = name;
+	}
+	
 /*	public static void main(String[] args) {
 		Student studentLee = new Student();
 		studentLee.studentName = "이순신";
--- src/classObjects/StudentTest.java
+++ src/classObjects/StudentTest.java
@@ -6,9 +6,22 @@ package classObjects; //패키지란?
 public class StudentTest {
 
 	public static void main(String[] args) {
-		Student studentLee = new Student();
+		
+		Student studentLee = new Student(); //인스턴스 생성 Student: 참조형 데이터타입
 		studentLee.studentName = "이순신";
+		studentLee.studentID = 100;
 		studentLee.address = "서울시 서초구 서초동";
+
+		
+		Student studentKim = new Student(); // studentKim은 스택 new Student 인스턴스는 힙메모리에 생성 (가비지콜렉터에서 없애줌)
+		studentKim.studentName = "김유";
+		studentKim.studentID = 100;
+		studentKim.address = "미국 산호세";
+		
 		studentLee.showStudentInfor();
+		studentKim.showStudentInfor();
+		
+		System.out.println(studentLee);
+		System.out.println(studentKim);
 		}
 }
