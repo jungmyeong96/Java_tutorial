a034da27f63b4c7a7c03c2d7221ca053b9749c54
한중명 <gkswndaud@gmail.com>
2023-02-01 15:21:08 +0900

    information hiding

---
+++ src/hiding/BirthDay.java
@@ -0,0 +1,37 @@
+package hiding;
+
+public class BirthDay {
+	private int day;
+	private int month;
+	private int year;
+	
+	
+	public int getDay() {
+		return day;
+	}
+	public void setDay(int day) {
+		if (month == 2) {
+			if (day < 1 || day > 28)
+				System.out.println("날짜 오류입니다 ");
+			else
+				this.day = day;
+				
+		}
+		else {
+			this.day = day;		
+		}
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+}
---
+++ src/hiding/BirthDayTest.java
@@ -0,0 +1,20 @@
+package hiding;
+
+public class BirthDayTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BirthDay day = new BirthDay();
+		
+//		day.month = 2;
+//		day.year = 2018;
+//		day.day = 30;
+		day.setMonth(2);
+		day.setDay(30);
+
+		day.setYear(1996);
+		
+
+	}
+
+}
